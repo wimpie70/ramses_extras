ad556adc76c81d444fb8f6a0e710eacf4a0a66c1
Willem <willem@homeassistant.local>
2025-12-23 13:01:53 +0100

    bump to 0.10.2

--- docs/RAMSES_EXTRAS_ARCHITECTURE.md
+++ docs/RAMSES_EXTRAS_ARCHITECTURE.md
@@ -1,4 +1,4 @@
-**Document Version:** 0.10.1
+**Document Version:** 0.10.2
 
 # 1. Table of Contents
 - [1. Table of Contents](#1-table-of-contents)
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="ramses_extras",
-    version="0.10.1",
+    version="0.10.2",
     packages=[
         "custom_components.ramses_extras",
         "custom_components.ramses_extras.framework",
