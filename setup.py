0e18952e70ec27476793377b422bb1d1f90cdc19
Willem <willem@homeassistant.local>
2026-09-05 11:52:03 +0200

    bump to 0.20.12

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="ramses_extras",
-    version="0.21.11",
+    version="0.21.12",
     packages=[
         "custom_components.ramses_extras",
         "custom_components.ramses_extras.framework",
